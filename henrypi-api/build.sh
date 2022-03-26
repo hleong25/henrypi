41a7dca70e7b1a68ae1de4514dcdc7003fae5ab9
Henry Leong <hleong25@gmail.com>
2022-03-25 23:53:05 -0700

    fetch from specific platform, push henrypi-api

---
+++ henrypi-api/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker login
+
+docker buildx build --platform=linux/amd64 -t hleong25/henrypi-api:amd64 --push .
+docker buildx build --platform=linux/arm64 -t hleong25/henrypi-api:arm64 --push .
