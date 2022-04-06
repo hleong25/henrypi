5eb57f82906eb556a6f5e6ee716377d2b799dae8
Henry Leong <hleong25@gmail.com>
2022-04-06 15:27:30 -0700

    add second video

--- henrypi-ui/src/App.js
+++ henrypi-ui/src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src="/api/video/9000/snapshot" className="snapshot9000" alt="snapshot9000" />
         <img src="/api/video/9000/stream" className="stream9000" alt="stream9000" />
+
+        <img src="/api/video/9001/snapshot" className="snapshot9001" alt="snapshot9001" />
+        <img src="/api/video/9001/stream" className="stream9001" alt="stream9001" />
       </header>
     </div>
   );
